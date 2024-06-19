906bfbfee0ad66f45f0265550d2c0474d2658ac3
bruno santana <bruno17henrique@gmail.com>
2024-06-18 21:49:37 -0300

    endpoint create para tecnico

--- src/main/java/com/bruno/helpdesk/services/TecnicoService.java
+++ src/main/java/com/bruno/helpdesk/services/TecnicoService.java
@@ -1,11 +1,13 @@
 package com.bruno.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bruno.helpdesk.domain.Tecnico;
+import com.bruno.helpdesk.domain.dtos.TecnicoDTO;
 import com.bruno.helpdesk.repositories.TecnicoRepesitory;
 import com.bruno.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -13,10 +15,20 @@ import com.bruno.helpdesk.services.exceptions.ObjectnotFoundException;
 public class TecnicoService {
 
 	@Autowired
-	private TecnicoRepesitory repesitory;
+	private TecnicoRepesitory repository;
 	
 	public Tecnico findById(Integer id) {
-		Optional<Tecnico> obj = repesitory.findById(id);
+		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id));
+	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 }
