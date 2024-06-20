946d37cc525fdc043dbaac2ca38ebadc9859e251
bruno santana <bruno17henrique@gmail.com>
2024-06-19 21:28:23 -0300

    Validation

--- src/main/java/com/bruno/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/bruno/helpdesk/domain/dtos/TecnicoDTO.java
@@ -10,15 +10,25 @@ import com.bruno.helpdesk.domain.Tecnico;
 import com.bruno.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.validation.constraints.NotNull;
+
 public class TecnicoDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
 	
 	protected Integer id;
+	
+	@NotNull(message = "O campo nome é requerido!")
     protected String nome;
+	
+	@NotNull(message = "O campo cpf é requerido!")
     protected String cpf;
+	
+	@NotNull(message = "O campo email é requerido!")
     protected String email;
+	
+	@NotNull(message = "O campo senha é requerido!")
     protected String senha;
     protected Set<Integer> perfis = new HashSet<>();
 
--- src/main/java/com/bruno/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/bruno/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,8 @@ package com.bruno.helpdesk.resources.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,7 +18,8 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ObjectnotFoundException.class)
 	public ResponseEntity<StandarError> objectnotFoundException(ObjectnotFoundException ex, HttpServletRequest request){
 		
-		StandarError error = new StandarError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Object Not Found", ex.getMessage(), request.getRequestURI());
+		StandarError error = new StandarError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), 
+				"Object Not Found", ex.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 		
@@ -25,9 +28,24 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(DataintegrityViolationException.class)
 	public ResponseEntity<StandarError> dataintegrityViolationException(DataintegrityViolationException ex, HttpServletRequest request){
 		
-		StandarError error = new StandarError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Violação de dados!", ex.getMessage(), request.getRequestURI());
+		StandarError error = new StandarError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), 
+				"Violação de dados!", ex.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandarError> validationErrors(MethodArgumentNotValidException ex, HttpServletRequest request){
+		
+		ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), 
+				"Erro de validação!", "Error na validação dos campos!", request.getRequestURI());
+		
+		for (FieldError x : ex.getBindingResult().getFieldErrors()) {
+			errors.addError(x.getField(), x.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 		
 	}
 }
