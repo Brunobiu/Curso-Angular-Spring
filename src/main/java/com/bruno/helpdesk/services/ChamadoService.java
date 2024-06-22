23f3419cc01c4aa48f6025bb200aacdcf87ef356
bruno santana <bruno17henrique@gmail.com>
2024-06-22 15:03:45 -0300

    FindById para chamado

---
+++ src/main/java/com/bruno/helpdesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.bruno.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bruno.helpdesk.domain.Chamado;
+import com.bruno.helpdesk.repositories.ChamadoRepesitory;
+import com.bruno.helpdesk.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class ChamadoService {
+
+	@Autowired
+	private ChamadoRepesitory repesitory;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repesitory.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado ID:" + id));
+	}
+}
